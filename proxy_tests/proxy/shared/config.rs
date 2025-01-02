2bc21dff83d765c328545d722aca6a3963f7a3bd
Calvin Neo <calvinneo1995@gmail.com>
2025-01-02 15:33:12 +0800

    fix tests
    Signed-off-by: Calvin Neo <calvinneo1995@gmail.com>

--- proxy_tests/proxy/shared/config.rs
+++ proxy_tests/proxy/shared/config.rs
@@ -141,7 +141,7 @@ fn test_default_no_config_item() {
     assert_eq!(config.server.status_thread_pool_size, 2);
 
     assert_eq!(config.raft_store.evict_cache_on_memory_ratio, 0.1);
-    assert_eq!(config.memory_usage_high_water, 0.1);
+    assert_eq!(config.memory_usage_high_water, 0.9);
     assert_eq!(config.server.reject_messages_on_memory_ratio, 0.05);
 
     assert_eq!(config.raft_store.enable_v2_compatible_learner, true);
